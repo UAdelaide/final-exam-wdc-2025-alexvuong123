5a48666c2bded2cf37ebea0a76201fb0305458cf
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 11:44:05 +0000

    [automatic commit] 2025-06-20 11:44:05 UTC 103.237.18.24

--- part1/app.js
+++ part1/app.js
@@ -83,7 +83,7 @@ app.get('/api/dogs', async (req, res) => {
 // /api/walkrequests/open
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [rows] = await db.execute(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.request_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
+    const [rows] = await db.execute(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
       FROM WalkRequests
       JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
       JOIN Users ON Dogs.owner_id = Users.user_id

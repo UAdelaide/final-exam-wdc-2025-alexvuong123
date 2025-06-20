cb6d92b685361be564273c3b0e7fbc8f5bd1fc12
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 14:16:19 +0000

    [automatic commit] 2025-06-20 14:16:19 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -61,7 +61,7 @@ router.post('/login', async (req, res) => {
 router.post('/logout', async (req, res) => {
 req.session.destroy((err) =>{
   res.clearCookie('connect.sid');
-})
+});
 });
 
 module.exports = router;

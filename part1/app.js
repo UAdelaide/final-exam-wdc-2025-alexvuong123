a0a51df849c0f2440f02dc8422e7dd95ea023189
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 12:08:01 +0000

    [automatic commit] 2025-06-20 12:08:00 UTC ;; communications error to 208.67.222.222#53: timed out 103.237.18.24

--- part1/app.js
+++ part1/app.js
@@ -80,23 +80,17 @@ let db;
       `);
             await db.execute(`
        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-       VALUES ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', '45', 'Beachside Ave', 'accepted'),
+       VALUES ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', '45', 'Beachside Ave', 'accepted')
       `);
             await db.execute(`
        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-       VALUES ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', '30', 'Parklands', 'open'),
-       ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', '45', 'Beachside Ave', 'accepted'),
-       ((SELECT dog_id FROM Dogs WHERE name='Bob'), '2025-06-10 10:15:00', '15', 'North Terrace', 'cancelled'),
-       ((SELECT dog_id FROM Dogs WHERE name='Kevin'), '2025-06-10 10:30:00', '30', 'King William St', 'completed'),
-       ((SELECT dog_id FROM Dogs WHERE name='Jason'), '2025-06-10 11:00:00', '20', 'Grenfell St', 'open');
+       VALUES
+       ((SELECT dog_id FROM Dogs WHERE name='Bob'), '2025-06-10 10:15:00', '15', 'North Terrace', 'cancelled')
       `);
             await db.execute(`
        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-       VALUES ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', '30', 'Parklands', 'open'),
-       ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', '45', 'Beachside Ave', 'accepted'),
-       ((SELECT dog_id FROM Dogs WHERE name='Bob'), '2025-06-10 10:15:00', '15', 'North Terrace', 'cancelled'),
-       ((SELECT dog_id FROM Dogs WHERE name='Kevin'), '2025-06-10 10:30:00', '30', 'King William St', 'completed'),
-       ((SELECT dog_id FROM Dogs WHERE name='Jason'), '2025-06-10 11:00:00', '20', 'Grenfell St', 'open');
+       VALUES
+       ((SELECT dog_id FROM Dogs WHERE name='Kevin'), '2025-06-10 10:30:00', '30', 'King William St', 'completed')
       `);
             await db.execute(`
        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)

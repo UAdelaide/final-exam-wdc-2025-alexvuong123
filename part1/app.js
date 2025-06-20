db8e8b9451548a1016ca145dae7daa3ca812734a
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 09:06:25 +0000

    [automatic commit] 2025-06-20 09:06:25 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/app.js
+++ part1/app.js
@@ -94,7 +94,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
     const [rows] = await db.execute(`SELECT WalkRequests.requests_id, Dog.name AS dog_name, WalkRequests.request_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
       FROM WalkRequests
       JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
-      JOIN Users ON Dog
+      JOIN Users ON Dogs_owner_id = Users.user_id
+      WHERE WalkRequest
       `);
     res.json(rows);
   } catch (err) {

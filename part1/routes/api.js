b6bf162c18d3d2f127c414e884b839879e3c7562
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 04:03:41 +0000

    [automatic commit] 2025-06-20 04:03:41 UTC 103.237.18.24

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -6,12 +6,13 @@ let db;
 
 (async () => {
     try{
-        // insert 
+        // insert sample for dogs
          await db.execute(`
             INSERT INTO dogs (name, size, owner_id)
             VALUES ('Max', 'medium', (SELECT user_id FROM Users WHERE username='alice123')),
             ('Bella', 'small', (SELECT user_id FROM Users WHERE username='carol123'))
             `);
+         
     } catch (err){
         console.error('Error', err);
     }

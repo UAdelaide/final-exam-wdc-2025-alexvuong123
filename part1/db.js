ada29e52e2a2524fc1b050a35fb92a5161d81878
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 02:45:03 +0000

    [automatic commit] 2025-06-20 02:45:03 UTC ;; communications error to 208.67.222.222#53: timed out 103.237.18.24

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-
-
-})
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'DogWalkService'
+});
 module.exports = db;

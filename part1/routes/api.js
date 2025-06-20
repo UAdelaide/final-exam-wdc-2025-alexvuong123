99d3101adcae1ebc0d2b9995a67abd8ddd5026ec
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 04:18:07 +0000

    [automatic commit] 2025-06-20 04:18:07 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -23,7 +23,7 @@ let db;
         // insert sample for open walk requests
          await db.execute(`
             INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location)
-s
+            VALUES ((SELECT ))
             `)
     } catch (err){
         console.error('Error', err);

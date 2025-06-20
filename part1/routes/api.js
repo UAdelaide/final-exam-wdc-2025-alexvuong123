a575a71d0a2fdc879b1d08628cfc064cd8b545be
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 05:01:21 +0000

    [automatic commit] 2025-06-20 05:01:20 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 103.237.18.24

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -31,7 +31,7 @@ var db = require('../db'); // database
         // insert sample for walkers summary
         await db.execute(`
             INSERT INTO WalkRatings(request_id, walker_id, owner_id, rating)
-            VALUES (1, 2, 2),
+            VALUES (SELECT ),
             (2, 3, 4)
             `);
     } catch (err){

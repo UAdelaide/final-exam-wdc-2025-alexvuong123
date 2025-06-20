2c8b98252b0c9429f78e892f80cc2320d83f44a3
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 12:04:59 +0000

    [automatic commit] 2025-06-20 12:04:59 UTC ;; communications error to 208.67.222.222#53: timed out 103.237.18.24

--- part1/app.js
+++ part1/app.js
@@ -39,29 +39,12 @@ let db;
       await db.execute(`
        INSERT INTO Users (username, email, password_hash, role)
        VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-
+       ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+       ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+       ('newalker', 'newalker@example.com', 'hashed246', 'walker'),
+       ('johndoe', 'johndoe@example.com', 'hashed369', 'owner');
       `);
-             await db.execute(`
-       INSERT INTO Users (username, email, password_hash, role)
-    ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
-        `);
-
-               await db.execute(`
-       INSERT INTO Users (username, email, password_hash, role)
-       ('newalker', 'newalker@example.com', 'hashed246', 'walker')
-        `);
-
-               await db.execute(`
-       INSERT INTO Users (username, email, password_hash, role)
-         ('johndoe', 'johndoe@example.com', 'hashed369', 'owner')
-        `);
-
-
-       await db.execute(`
-       INSERT INTO Users (username, email, password_hash, role)
-         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        `);
-
+   
       await db.execute(`
        INSERT INTO Dogs (owner_id, name, size)
        VALUES ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),

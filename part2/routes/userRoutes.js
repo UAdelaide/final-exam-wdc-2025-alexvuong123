a4dba6765d23b4eb856ac695d096109ee5147ce8
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 13:13:19 +0000

    [automatic commit] 2025-06-20 13:13:18 UTC 103.237.18.24

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -36,7 +36,7 @@ router.get('/me', (req, res) => {
 });
 
 // POST login (dummy version)
-router.post('/users', async (req, res) => {
+router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {

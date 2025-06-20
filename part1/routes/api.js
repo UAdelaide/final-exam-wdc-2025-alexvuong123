be68bd71ae7d13ca6a2cc77cc12f6dd2a305271b
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 03:05:43 +0000

    [automatic commit] 2025-06-20 03:05:43 UTC 103.237.18.24

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db'); // database
 
+// /api/dogs
 
+
+// /api/walkrequests/open
+
+
+// /api/walkers/summary
 
 
 module.exports = router;

4aaa8121dd91ea18de5d142777366390a778b750
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 10:39:21 +0000

    [automatic commit] 2025-06-20 10:39:21 UTC ;; communications error to 208.67.222.222#53: timed out 103.237.18.24

--- part1/app.js
+++ part1/app.js
@@ -98,7 +98,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 // /api/walkers/summary
 app.get('/api/walkers/summary', async (req, res) => {
   try {
-    const [rows] = await db.execute(`SELECT Users.username AS walker_username, COUNT(WalkRatings.rating_id) AS total_ratings, AVG(WalkRatings.rating) AS average_rating
+    const [rows] = await db.execute(`SELECT Users.username AS walker_username, COUNT(WalkRatings.rating_id) AS total_ratings, AVG(WalkRatings.rating) AS average_rating,
       (SELECT COUNT(*) FROM WalkRequests JOIN WalkApplications ON WalkRequests.request_id = WalkApplications.request_id
       WHERE WalkApplications.walker_id = Users.user_id AND WalkApplications.status = 'completed') AS completed_walks
       FROM Users LEFT JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id

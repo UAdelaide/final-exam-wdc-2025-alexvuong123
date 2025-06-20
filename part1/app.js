47ded3bd8caf796a1a686c0748d5566af808447c
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 12:01:25 +0000

    [automatic commit] 2025-06-20 12:01:25 UTC 103.237.18.24

--- part1/app.js
+++ part1/app.js
@@ -55,10 +55,10 @@ let db;
 
       await db.execute(`
        INSERT INTO Dogs (owner_id, name, size)
-       VALUES ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
+       ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
        `);
 
-       
+
       await db.execute(`
        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
        VALUES ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', '30', 'Parklands', 'open'),

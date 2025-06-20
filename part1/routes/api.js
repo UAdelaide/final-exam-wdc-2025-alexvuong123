4013f21c81321d91cb8daf4c69793c1f418c3147
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 03:22:22 +0000

    [automatic commit] 2025-06-20 03:22:22 UTC 103.237.18.24

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -7,7 +7,7 @@ router.get('/dogs', async(req, res) => {
     try{
         const [rows] = await db.execute(`
             INSERT INTO dogs (name, size, owner_id)
-            VALUES ('Max', 'medium', (SELECT ))`)
+            VALUES ('Max', 'medium', (SELECT user_id FROM Users WHERE username='alice123'))`)
     }
 })
 

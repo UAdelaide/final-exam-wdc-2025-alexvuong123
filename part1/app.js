169eb68e37d46a7a2ebc62b5fc1df5f68f88e3de
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 11:55:11 +0000

    [automatic commit] 2025-06-20 11:55:11 UTC 103.237.18.24

--- part1/app.js
+++ part1/app.js
@@ -99,8 +99,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
 app.get('/api/walkers/summary', async (req, res) => {
   try {
     const [rows] = await db.execute(`SELECT Users.username AS walker_username, COUNT(WalkRatings.rating_id) AS total_ratings, AVG(WalkRatings.rating) AS average_rating,
-      (SELECT COUNT(*) FROM WalkRequests JOIN WalkRatings ON WalkRequests.request_id = WALKRatings.request_id
-      WHERE WALKRatings.walker_id = Users.user_id AND WalkRequests.status = 'completed') AS completed_walks
+      (SELECT COUNT(*) FROM WalkRequests JOIN WalkRatings ON WalkRequests.request_id = WalkRatings.request_id
+      WHERE WalkRatings.walker_id = Users.user_id AND WalkRequests.status = 'completed') AS completed_walks
       FROM Users LEFT JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id
       WHERE Users.role = 'walker' GROUP BY Users.user_id
       `);

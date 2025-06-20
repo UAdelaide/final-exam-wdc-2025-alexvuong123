482f42837a945ec48361ddecd338a9dfbacb9909
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 09:22:48 +0000

    [automatic commit] 2025-06-20 09:22:48 UTC 103.237.18.24

--- part1/app.js
+++ part1/app.js
@@ -32,8 +32,9 @@ let db;
         });
 
         // Create the database if it doesn't exist
-        await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+        await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
         await connection.end();
+
     // Now connect to the created database
     db = await mysql.createConnection({
       host: 'localhost',

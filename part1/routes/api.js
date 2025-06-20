c83fde7bfce5417e813922c0dbe802e6d80d591e
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 04:16:30 +0000

    [automatic commit] 2025-06-20 04:16:30 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 103.237.18.24

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -10,7 +10,8 @@ let db;
         // insert users
         await db.execute(`
             INSERT INTO Users (username, email, password_hash, role)
-            VALUES('('alice123', 'alice@example.com', 'hashed123', 'owner'),')
+            VALUES('alice123', 'alice@example.com', 'hashed123', 'owner'),
+            (')
             `)
 
         // insert sample for dogs

74a06887ec1eb912db7c1a7ca291c5606ef4c42c
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-20 08:56:46 +0000

    [automatic commit] 2025-06-20 08:56:46 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/app.js
+++ part1/app.js
@@ -74,10 +74,10 @@ let db;
   }
 })();
 
-// Route to return books as JSON
-app.get('/', async (req, res) => {
+// /api/dogs
+app.get('/api/dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
+    const [dogs] = await db.execute('SELECT * FROM books');
     res.json(books);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });

d06f4cc22e0f2d1eb08a0f269c658d17a69469f4
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-21 00:05:40 +0000

    [automatic commit] 2025-06-21 00:05:40 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part2/app.js
+++ part2/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var session = require('express-session');
+const cookieParser = require('cookie-parser');
 const path = require('path');
 require('dotenv').config();
 

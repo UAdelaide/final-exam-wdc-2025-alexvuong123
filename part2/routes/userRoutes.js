346145e639627c7e876687bfe229f59802e348b9
alexvuong123 <a1927162@adelaide.edu.au>
2025-06-21 01:56:13 +0000

    [automatic commit] 2025-06-21 01:56:13 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 103.237.18.24

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -15,9 +15,9 @@ router.get('/', async (req, res) => {
 // GET all dogs
 router.get('/dogs', async (req, res) => {
   try {
-    const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?'
-      
-    )
+    const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?',
+      [req.session.userId]
+    );
     res.json(rows);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
